d7a6ff9f24bb9681ae6d7730a13253d37c965db8
manoj gaikwad <gaikwadm853@gmail.com>
2021-07-27 15:25:13 +0530

    Added: Create doublet combination and store and show the percentage of winning

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -5,7 +5,7 @@ echo "Welcome to flip coin combination program"
 declare -A singlet
 heads=0
 tails=0
-loop=10
+loop=100
 
 for (( i=1; i<=$loop; i++ ))
 do
@@ -21,11 +21,54 @@ do
     fi
 done
 
-
 echo "Heads won $heads"
 echo "tails won $tails"
-echo 
 hp=$(($heads*100/$loop))
 tp=$(($tails*100/$loop))
 echo "Heads Percent : $hp%"
 echo "Tails Percent : $tp%"
+
+declare -A doublet
+
+hh=0
+ht=0
+th=0
+tt=0
+
+for((i=0;i<=$loop;i++))
+do
+	random=$((($RANDOM%4)+1))
+        case $random in
+		1)
+			((hh++))
+        	doublet[$random]=$((${doublet[$random]}+1))
+	    ;;
+		2)
+			((ht++))
+        	doublet[$random]=$((${doublet[$random]}+1))
+	    ;;
+		3)
+			((th++))
+        	doublet[$random]=$((${doublet[$random]}+1))
+	    ;;
+		4)
+			((tt++))
+        	doublet[$random]=$((${doublet[$random]}+1))
+	    ;;
+    esac
+done
+echo
+echo "hh won $hh"
+echo "ht won $ht"
+echo "th won $th"
+echo "tt won $tt"
+
+hhp=$(($hh*100/$loop))
+htp=$(($ht*100/$loop))
+thp=$(($th*100/$loop))
+ttp=$(($tt*100/$loop))
+
+echo "HH Percent : $hhp%"
+echo "HT Percent : $htp%"
+echo "TH Percent : $thp%"
+echo "TT Percent : $ttp%"
