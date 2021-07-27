f0355247db4cb3381c04a7834228a98d9963bf0e
manoj gaikwad <gaikwadm853@gmail.com>
2021-07-27 14:50:38 +0530

    Added: Store result in dictionary and show percentage of winning

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,11 +2,30 @@
 
 echo "Welcome to flip coin combination program"
 
-res=$((RANDOM%2))
+declare -A singlet
+heads=0
+tails=0
+loop=10
 
-if [ $res -eq 0 ]
-then
-    echo "Heads win"
-else
-    echo "Tails win"
-fi
+for (( i=1; i<=$loop; i++ ))
+do
+    res=$((RANDOM%2))
+
+    if [ $res -eq 0 ]
+    then
+        singlet[$res]=$((${singlet[$res]}+1))
+        ((heads++))
+    else
+        singlet[$res]=$((${singlet[$res]}+1))
+        ((tails++))
+    fi
+done
+
+
+echo "Heads won $heads"
+echo "tails won $tails"
+echo 
+hp=$(($heads*100/$loop))
+tp=$(($tails*100/$loop))
+echo "Heads Percent : $hp%"
+echo "Tails Percent : $tp%"
